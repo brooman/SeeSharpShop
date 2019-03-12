f18edf4910e0091a88d3c3a57e3bd19e5bd398f9
André Broman <laykith@gmail.com>
2019-03-12 14:38:54 +0100

    Added Empty StoreController

---
+++ SeeSharpShop/Controllers/StoreController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeeSharpShop.Controllers
+{
+    [Route("api/[controller]")]
+    public class StoreController : Controller
+    {
+    }
+}
