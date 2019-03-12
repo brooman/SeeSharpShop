c11d96eef6ba17d51260bb8460a57bf5e272099f
André Broman <laykith@gmail.com>
2019-03-12 14:29:54 +0100

    Added get product by id method to ProductsController

--- SeeSharpShop/Controllers/ProductsController.cs
+++ SeeSharpShop/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SeeSharpShop.Models;
@@ -9,12 +10,29 @@ namespace SeeSharpShop.Controllers
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
+
         [HttpGet]
         [ProducesResponseType(typeof(List<Product>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Get()
         {
             return Ok(Database);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var product = Database.First(item => item.Id == id);
+                return Ok(product);
+            }
+            catch(InvalidOperationException)
+            {
+                return NotFound();
+            }
         }
 
         private static readonly List<Product> Database = new List<Product> { 
