c98a2a0cc824d1e79fcf45ae0fe42893d78ee864
André Broman <laykith@gmail.com>
2019-03-12 14:11:50 +0100

    Added ShopController

---
+++ SeeSharpShop/Controllers/ShopController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeeSharpShop.Controllers
+{
+    [Route("api/[controller]")]
+    public class ShopController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
