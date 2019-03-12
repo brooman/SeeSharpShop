61486d7d07bb51e5eda200fb49136e54955929ca
André Broman <laykith@gmail.com>
2019-03-12 14:19:36 +0100

    Added Status codes and Dummy data

--- SeeSharpShop/Controllers/ShopController.cs
+++ SeeSharpShop/Controllers/ShopController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SeeSharpShop.Models;
 
 namespace SeeSharpShop.Controllers
 {
@@ -7,9 +10,32 @@ namespace SeeSharpShop.Controllers
     public class ShopController : Controller
     {
         [HttpGet]
+        [ProducesResponseType(typeof(List<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Get()
         {
-            return Ok("test");
+            return Ok(Database);
         }
+
+        private static readonly List<Product> Database = new List<Product> { 
+            new Product
+            {
+                Id = 0,
+                Name = "Glasses",
+                Cost = 10
+            },
+            new Product
+            {
+                Id = 1,
+                Name = "Cool Glasses",
+                Cost = 10
+            },
+            new Product
+            {
+                Id = 2,
+                Name = "Uncool Glasses",
+                Cost = 10
+            }
+        };
     }
 }
