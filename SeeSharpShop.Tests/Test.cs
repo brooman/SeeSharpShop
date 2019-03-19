bb262fafc4d28e987abf2d44a602c60680004fb1
André Broman <laykith@gmail.com>
2019-03-19 13:03:59 +0100

    Added test structure

---
+++ SeeSharpShop.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using SeeSharpShop.Controllers;
+using System;
+namespace SeeSharpShop.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
