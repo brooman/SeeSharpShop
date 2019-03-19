faa3db1dcc2cee6b54fde06b77b4b58780b6075d
André Broman <laykith@gmail.com>
2019-03-19 13:24:16 +0100

    Changed from list to mysql database

--- SeeSharpShop/Controllers/ProductsController.cs
+++ SeeSharpShop/Controllers/ProductsController.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using SeeSharpShop.Models;
+using Dapper;
+using MySql.Data.MySqlClient;
 
 namespace SeeSharpShop.Controllers
 {
+
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
+        protected readonly string connectionString;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            this.connectionString = configuration.GetConnectionString("ConnectionString");
+        }
 
         [HttpGet]
         [ProducesResponseType(typeof(List<Product>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Index()
         {
-            return Ok(Database);
+            using (var connection = new MySqlConnection(this.connectionString))
+            {
+                var Products = connection.Query<Product>("SELECT * FROM products").ToList();
+                return Ok(Products);
+            }
+
         }
 
         [HttpGet("{id}")]
@@ -26,34 +41,16 @@ namespace SeeSharpShop.Controllers
         {
             try
             {
-                var product = Database.First(item => item.Id == id);
-                return Ok(product);
+                using (var connection = new MySqlConnection(this.connectionString))
+                {
+                    var Product = connection.Query<Product>("SELECT * FROM products WHERE id = @id", new { id });
+                    return Ok(Product);
+                }
             }
             catch(InvalidOperationException)
             {
                 return NotFound();
             }
         }
-
-        private static readonly List<Product> Database = new List<Product> { 
-            new Product
-            {
-                Id = 0,
-                Name = "Glasses",
-                Cost = 10
-            },
-            new Product
-            {
-                Id = 1,
-                Name = "Cool Glasses",
-                Cost = 10
-            },
-            new Product
-            {
-                Id = 2,
-                Name = "Uncool Glasses",
-                Cost = 10
-            }
-        };
     }
 }
