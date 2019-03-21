a261d60b3a8ff83fb01855341a8abc5fe1ae8760
André Broman <laykith@gmail.com>
2019-03-21 13:30:30 +0100

    Added ProductFactory

---
+++ SeeSharpShop.UnitTests/Factories/ProductFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using SeeSharpShop.Models;
+
+namespace SeeSharpShop.UnitTests.Factories
+{
+    public static class ProductFactory
+    {
+        public static List<Product> CreateCollection(int Count = 1)
+        {
+            var products = new List<Product>();
+
+            for(int i = 0; i < Count; i++)
+            {
+                products.Add(CreateSingle(i));
+            }
+
+            return products;
+        }
+
+        public static Product CreateSingle(int id = 1)
+        {
+            var random = new Random();
+
+            return new Product
+            {
+                Id = id,
+                Name = $"Test item {id}",
+                Description = $"Test description",
+                ImagePath = $"https://example.com/testimage.png",
+                Cost = random.Next(1, 1000),
+
+            };
+        }
+    }
+}
