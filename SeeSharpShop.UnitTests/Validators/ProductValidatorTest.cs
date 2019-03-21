c9bba40c42385dc229a731589e30331a42f21bce
André Broman <laykith@gmail.com>
2019-03-21 14:11:45 +0100

    Added Validator test

---
+++ SeeSharpShop.UnitTests/Validators/ProductValidatorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SeeSharpShop.UnitTests.Factories;
+using NUnit.Framework;
+using SeeSharpShop.Repositories;
+using SeeSharpShop.Services;
+using SeeSharpShop.Validators;
+using SeeSharpShop.Models;
+using FakeItEasy;
+
+namespace SeeSharpShop.UnitTests.Validators
+{
+    [TestFixture]
+    public class ProductValidatorTest
+    {
+        private IProductRepository productRepository;
+        private ProductService productService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.productRepository = A.Fake<IProductRepository>();
+            this.productService = new ProductService(this.productRepository);
+        }
+
+        [Test]
+        public void ValidProduct_ReturnsTrue()
+        {
+            var product = ProductFactory.CreateSingle(1);
+            var validator = new ProductValidator(productRepository);
+
+            var results = validator.Validate(product);
+
+            Assert.That(results.IsValid);
+        }
+
+    }
+}
