ff9edc693c7c4956868cd4aa0659296a189204d6
André Broman <laykith@gmail.com>
2019-03-21 13:31:05 +0100

    Added productRepository Test

---
+++ SeeSharpShop.UnitTests/Services/ProductServicesTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using SeeSharpShop.Repositories;
+using SeeSharpShop.Services;
+using FakeItEasy;
+using SeeSharpShop.Models;
+using SeeSharpShop.UnitTests.Factories;
+using System.Collections.Generic;
+
+namespace SeeSharpShop.UnitTests.Services
+{
+    public class ProductServicesTest
+    {
+        private IProductRepository productRepository;
+        private ProductService productService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.productRepository = A.Fake<IProductRepository>();
+            this.productService = new ProductService(this.productRepository);
+        }
+
+        [Test]
+        public void All_ReturnsResultFromRepository()
+        {
+            var products = ProductFactory.CreateCollection(10);
+            A.CallTo(() => this.productService.All()).Returns(products);
+
+            var result = this.productService.All();
+
+            CollectionAssert.AllItemsAreInstancesOfType(result, typeof(Product));
+            Assert.That(result, Is.EqualTo(products));
+        }
+    }
+}
